9d228676ec95d03c6d6725d0f69aaddd1e3d59b5
deliteser112 <deliteser112@gmail.com>
2024-07-19 19:23:14 +0200

    UI updated

---
+++ dashboard-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="bg-blue-500 min-h-screen flex flex-col items-center justify-center text-white">
+        <h1 className="text-4xl font-bold">Welcome to React with TypeScript and Tailwind CSS!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
